b9ffd0390a7ca90688fc301bef814ed1e0bccf78
Christopher <chrismartinez.2054@gmail.com>
2023-08-15 23:28:37 -0600

    Finished PanelBear

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+  {
+    productName: "Python Tutorial",
+    productNumber: "4345",
+    paymentStatus: "Due",
+    status: "Pending",
+  },
+  {
+    productName: "Sass Course",
+    productNumber: "9745",
+    paymentStatus: "Refunded",
+    status: "Declined",
+  },
+  {
+    productName: "Java Tutorial",
+    productNumber: "3357",
+    paymentStatus: "Paid",
+    status: "Active",
+  },
+];
